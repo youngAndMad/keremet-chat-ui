16bd5cba3d1b7249da70e5f3065fa6f65d89af38
youngAndMad <kkraken2005@gmail.com>
2024-04-09 00:12:05 +0500

    tailwind init and react hook form for login page

--- src/routes/auth/login/index.tsx
+++ src/routes/auth/login/index.tsx
@@ -1,64 +1,97 @@
-import {useState} from "react";
-import React from "react";
-import './Login.scss'
-import githubLogo from '@assets/images/logo/github.png'
-import api from '@libs/api'
-import {createFileRoute, Link} from '@tanstack/react-router'
+import "./Login.scss";
+import githubLogo from "@assets/images/logo/github.png";
+import api from "@libs/api";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/auth/login/')({
-    component: Login
-})
+export const Route = createFileRoute("/auth/login/")({
+  component: Login,
+});
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
 
 function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>();
 
-    // api.get('/api/v1/auth/me')
-    //     .then(console.log)
+  const onSubmit: SubmitHandler<LoginFormData> = (data) => {
+    console.log(data);
+    // api
+    //   .post("/api/v1/auth/login", { ...data })
+    //   .then((data) => console.log(data))
+    //   .catch((err) => console.error(err));
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-
-        api.post('/api/v1/auth/login', {email, password})
-            .then(data => console.log(data))
-            .catch(err => console.error(err))
-    }
-
-    return (
-        <div className="login-container">
-            <div className="login-content">
-                <h1 className="login-title">Login to KeremetChat</h1>
-                <div className="social-login">
-                    <a className="btn btn-block social-btn github" href={import.meta.env.VITE_GITHUB_AUTH_URL}>
-                        <img src={githubLogo} alt="Github"/> Log in with Github</a>
-                </div>
-                <div className="or-separator">
-                    <span className="or-text">OR</span>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="form-item">
-                        <input type="email" name="email"
-                               className="form-control" placeholder="Email"
-                               value={email}
-                               onChange={(e) => setEmail(e.target.value)}
-                               required/>
-                    </div>
-                    <div className="form-item">
-                        <input type="password" name="password"
-                               className="form-control" placeholder="Password"
-                               value={password}
-                               onChange={(e) => setPassword(e.target.value)}
-                               required
-                        />
-                    </div>
-                    <div className="form-item">
-                        <button type="submit" className="btn btn-block btn-primary">Login</button>
-                    </div>
-                </form>
-                <span className="signup-link">New user? <Link to="/auth/signup">Sign up!</Link></span>
-            </div>
+  return (
+    <div className="login-container">
+      <div className="login-content">
+        <h1 className="login-title">Login to KeremetChat</h1>
+        <div className="social-login">
+          <a
+            className="btn btn-block social-btn github"
+            href={import.meta.env.VITE_GITHUB_AUTH_URL}
+          >
+            <img src={githubLogo} alt="Github" /> Log in with Github
+          </a>
         </div>
-    )
+        <div className="or-separator">
+          <span className="or-text">OR</span>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-item">
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-item">
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                maxLength: {
+                  value: 30,
+                  message: "Password cannot exceed 30 characters",
+                },
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters long",
+                },
+              })}
+              name="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
+          {/* Display error messages for password field */}
+          {errors.password && (
+            <p role="error" className="text-red-500 text-xs italic">
+              {errors.password.message}
+            </p>
+          )}
+          <div className="form-item">
+            <button type="submit" className="btn btn-block btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+        <span className="signup-link">
+          New user? <Link to="/auth/signup">Sign up!</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,37 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),TanStackRouterVite()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:9000',
-                changeOrigin: true,
-                secure: false,
-            }
-        }
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
-    resolve:{
-        alias:{
-            "@": path.resolve(__dirname, "./src/"),
-            "@pages": path.resolve(__dirname, "./src/pages/"),
-            "@components": path.resolve(__dirname, "./src/components/"),
-            "@ui": path.resolve(__dirname, "./src/components/ui"),
-            "@utils": path.resolve(__dirname, "./src/utils/"),
-            "@hooks": path.resolve(__dirname, "./src/hooks/"),
-            "@assets": path.resolve(__dirname, "./src/assets/"),
-            "@libs": path.resolve(__dirname, "./src/libs/"),
-            "@api" : path.resolve(__dirname, "./src/libs/api"),
-        }
-    }
-})
+  },
+  plugins: [react(), TanStackRouterVite()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "@pages": path.resolve(__dirname, "./src/pages/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+      "@utils": path.resolve(__dirname, "./src/utils/"),
+      "@hooks": path.resolve(__dirname, "./src/hooks/"),
+      "@assets": path.resolve(__dirname, "./src/assets/"),
+      "@libs": path.resolve(__dirname, "./src/libs/"),
+      "@api": path.resolve(__dirname, "./src/libs/api"),
+    },
+  },
+});
